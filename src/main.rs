5017656882b0b8165610081c192457e65fc18fa8
Jeroen Knoops <jeroen.knoops@gmail.com>
2021-03-07 14:49:07 +0100

    add clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+extern crate clap;
+use clap::{crate_authors, crate_description, crate_name, crate_version, App};
+
 fn main() {
-    println!("pcache");
+    App::new(crate_name!())
+        .about(crate_description!())
+        .version(crate_version!())
+        .author(crate_authors!())
+        .get_matches();
 }
